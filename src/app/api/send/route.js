d0c5040b751c1b576e79e4809b634bccf563573a
MyshinskyIlya <myshinskiyyy@gmail.com>
2023-12-04 23:02:14 +0400

    "created Footer"

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,23 +1,23 @@
-import { EmailTemplate } from "../../components/EmailTemplate";
-import { Resend } from "resend";
+// import { EmailTemplate } from "../../components/EmailTemplate";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async (req, res) => {
-    try {
-        const data = await resend.emails.send({
-            from: "Ilya <myshinskiyyy@gmail.com>",
-            to: ["myshinskiyyy@gmail.com"],
-            subject: "Hello world",
-            react: (
-                <>
-                    <p>Email Body</p>
-                </>
-            ),
-        });
+// export default async (req, res) => {
+//     try {
+//         const data = await resend.emails.send({
+//             from: "Ilya <myshinskiyyy@gmail.com>",
+//             to: ["myshinskiyyy@gmail.com"],
+//             subject: "Hello world",
+//             react: (
+//                 <>
+//                     <p>Email Body</p>
+//                 </>
+//             ),
+//         });
 
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(400).json(error);
-    }
-};
+//         res.status(200).json(data);
+//     } catch (error) {
+//         res.status(400).json(error);
+//     }
+// };
