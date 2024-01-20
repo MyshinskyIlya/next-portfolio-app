63ef153d88251884ec818030500a219b0969b641
MyshinskyIlya <myshinskiyyy@gmail.com>
2024-01-20 08:50:45 +0400

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "Hello",
-    assetPrefix: "Hi!Hi!Hi!",
+    basePath: "",
+    assetPrefix: "",
     // Другие настройки...
 };
 
